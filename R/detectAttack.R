1636139c5c0ce010170f22484f544d0643e6322d
Owen Sims <owen.sims@cyberlytic.com>
2018-07-09 14:28:58 +0100

    Clean up tests and README

--- R/detectAttack.R
+++ R/detectAttack.R
@@ -1,21 +1,21 @@
 #' @title Detect Attack
 #'
-#' @description This function takes in an argument and analyses it for malicious strings. 
+#' @description This function takes in a payload and analyses it for malicious strings. 
 #'  If malicious strings are found it will return an indicator and tokenized data (fingerprint).
 #'
-#' @details Detect SQLi or XSS in parameter or request body argument.
+#' @details Detect SQLi or XSS in parameter or request body payload.
 #'  The process of detection and classification is as follows:
 #'   \itemize{
-#'       \item{Send argument to be tested for SQL}
-#'       \item{Send argument to be tested for XSS}
+#'       \item{Send payload to be tested for SQL}
+#'       \item{Send payload to be tested for XSS}
 #'       \item{If malicious activities have been detected then return with a `1`}
 #'       \item{If no malicious activity has been detected then return with a `0`}
 #'   }
 #'
-#' @param argument A string containing a URL-decoded parameter, request body,
+#' @param payload A string containing a URL-decoded parameter, request body,
 #'  or header argument.
 #'
-#' @return Returns a binary integer indicating whether the argument contained a
+#' @return Returns a binary integer indicating whether the payload contained a
 #'  malicious element (`1`) or not (`0`).
 #'  
 #' @importFrom magrittr %>% %<>% %$% %T>%
@@ -25,20 +25,20 @@
 #' @export
 
 
-detectAttack <- function(argument) {
-  argumentClean <- iconv(
-    x = argument,
+detectAttack <- function(payload) {
+  payloadClean <- iconv(
+    x = payload,
     from = "ISO-8859-1",
     to = "UTF-8")
   
-  argumentLength <- argumentClean %>%
+  payloadLength <- payloadClean %>%
     nchar
   
   # Detect for SQLi
   isSQLi <- .C(
-    injectoR:::libinjection_sqli,
-    s = argumentClean,
-    slen = argumentLength,
+    libinjection_sqli,
+    s = payloadClean,
+    slen = payloadLength,
     b = 9L,
     fin0 = "",
     fin1 = "",
@@ -48,15 +48,15 @@ detectAttack <- function(argument) {
 
   # Detect for XSS
   isXSS <- .C(
-    injectoR:::libinjection_xss,
-    s = argumentClean,
-    len = argumentLength,
+    libinjection_xss,
+    s = payloadClean,
+    len = payloadLength,
     b = 9L)$b
 
   # Store results in a dataframe
   detectionResults <- data.frame(
-    argument = argumentClean,
-    length = argumentLength,
+    payload = payloadClean,
+    length = payloadLength,
     isSQLi = isSQLi$b,
     isXSS = isXSS,
     fingerprint = isSQLi[4:length(isSQLi)] %>%
--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-# `injectoR`: An `R` package for detecting SQLi and XSS attacks in arbitary strings
+# injectoR
+
+[![Build Status](https://travis-ci.com/O1sims/injectoR.svg?branch=master)](https://travis-ci.com/O1sims/injectoR)
 
 ## Overview
 
-The `injectoR` package provides a number of functions for the detection of SQLi and XSS commands in arbitary strings. This package basically serves as an R wrapper around the highly-regarded package [`libinjection`](https://github.com/client9/libinjection).
+The `injectoR` package provides a number of functions for the detection of SQLi and XSS commands in arbitary strings.
 
 ## Compiling SQLi and XSS filters to be used in R
 
